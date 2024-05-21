fb2df93f650cb293e5d628e16887f2b661647b4a
konish3 <ilya.konyshev0@mail.ru>
2024-05-21 14:39:56 +0300

    correction

--- js/index.js
+++ js/index.js
@@ -33,10 +33,11 @@ function updateTime() {
 const navProfile = document.getElementById("i_nav-profile");
 const navMap = document.getElementById("i_nav-map");
 const navTimer = document.getElementById("i_nav-timer");
+let map = document.getElementById("map");
 
-navMap.addEventListener("click", () =>
-  changeNav(navMap, [navProfile, navTimer])
-);
+navMap.addEventListener("click", () => {
+  changeNav(navMap, [navProfile, navTimer]);
+});
 navProfile.addEventListener("click", () =>
   changeNav(navProfile, [navTimer, navMap])
 );
@@ -81,8 +82,70 @@ document.addEventListener("touchstart", (e) => {
   touchstartX = e.changedTouches[0].screenX;
 });
 
-  document.addEventListener("touchend", (e) => {
-    touchendX = e.changedTouches[0].screenX;
-    checkDirection();
-  });
+document.addEventListener("touchend", (e) => {
+  touchendX = e.changedTouches[0].screenX;
+  checkDirection();
 });
+
+// Routing
+
+document.addEventListener("click", (e) => {
+  const { target } = e;
+  if (!target.matches("nav a")) {
+    return;
+  }
+  e.preventDefault();
+  route();
+});
+
+const routes = {
+  "/TT_WB/": "/TT_WB/pages/activity/activity.html",
+  "/TT_WB/map": "/TT_WB/pages/map/map.html",
+  "/TT_WB/timer": "/TT_WB/pages/timer/timer.html",
+};
+
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname.replace(/index\.html/, "");
+  const route = routes[path];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("content").innerHTML = html;
+  if (path === "/TT_WB/map") {
+    openMap();
+  }
+};
+
+window.addEventListener("popstate", handleLocation);
+
+window.route = route;
+
+handleLocation();
+
+// window.map = null;
+// const LOCATION = {
+//   center: [56.75351637162591, 37.16410006869976], // starting position [lng, lat]
+//   zoom: 17, // starting zoom
+// };
+// if (map) {
+//   async function main() {
+//     // Waiting for all api elements to be loaded
+//     await ymaps3.ready;
+//     const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+//     // Initialize the map
+//     map = new YMap(
+//       // Pass the link to the HTMLElement of the container
+//       document.getElementById("map"),
+//       // Pass the map initialization parameters
+//       { location: LOCATION, showScaleInCopyrights: true },
+//       // Add a map scheme layer
+//       [new YMapDefaultSchemeLayer({})]
+//     );
+//   }
+//   main();
+// }
